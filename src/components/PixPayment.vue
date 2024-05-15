5d8c798bcc66bda8c798e0c24e49904a80ef75f6
Stella <stellamaia97@hotmail.com>
2024-05-15 10:15:45 -0300

    Criação de Componentes

---
+++ src/components/PixPayment.vue
@@ -0,0 +1,20 @@
+<!-- <template>
+
+ </template> -->
+<!--
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useStore } from 'pinia'
+
+export default defineComponent({
+    setup() {
+        const store = useStore()
+
+        // Methods to handle credit card payment
+
+        return {
+            // Data and methods to be used in template
+        }
+    }
+})
+</script> -->
